d3d64e26de9a3c8be99a073b87b4641349be2a50
ymCarvalho <dudayasmim23@gmail.com>
2026-03-19 20:59:33 -0300

    primeiro commit

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,31 @@
+print("ola mundo")
+
+print(7 + 4)
+
+print("7 + 4")
+
+print("7" + "4")  #concatenação de strings
+
+#comentarios de 1 linha
+
+'''
+    comentários de múltiplas linhas 
+    
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+'''
+#Variaveis
+
+nome = "Alexandre"
+idade = 26
+peso = 70.2
+print(nome, idade, peso)
+print(f"Olá, {nome}!!!")
+
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+
+print(nome, idade, peso)
+print(idade + 1)
+
---
+++ aula02-variaveis-operadores/desafio1.py
@@ -0,0 +1,18 @@
+#1
+nome = input("Digite seu nome: ")
+print(f"Bem Vindo {nome}!!")
+
+
+#2
+dia = input("Digite seu dia de nascimento: ")
+mes = input("Digite seu mês de nascimento: ")
+ano = input("Digite seu ano de nascimento: ")
+print(f"Sua data de nascimento é: {dia}/{mes}/{ano}")
+
+
+#3
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+resultado = num1 + num2
+
+print(f"O resultado é: {num1 + num2}")
