7ab2a6928af18c8295b476a51a59b2b24874fe51
ymCarvalho <dudayasmim23@gmail.com>
2026-04-09 20:53:10 -0300

    novos entendimentos

---
+++ aula02-variaveis-operadores/aula2b-operadores.py
@@ -0,0 +1,31 @@
+import random
+
+num_rand = random.random()
+print (num_rand)
+
+num_rand2 = random.randint(1,10)
+
+print(num_rand2)
+
+
+nota = 7
+
+if nota <= 4:
+    print("reprovado")
+elif nota >= 5 | nota <=6:
+    print("rec")
+else:
+    print("Paso")
+
+
+
+
+
+
+
+
+
+
+
+
+
